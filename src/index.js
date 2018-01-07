af73cd27b515aa067cf09238584fb5e39d83ff4a
Itay Sidis <itaysidis@gmail.com>
2018-01-07 09:07:30 +0200

    ➕ Adding mongoose

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import express from "express";
 import path from "path";
-
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import auth from "./routes/auth";
 const app = express();
-
-app.post("/api/auth", (req, res) => {
-  res.status(400).json({ errors: { global: "invalid credentials" } });
-});
+app.use(bodyParser.json());
+mongoose.connect("mongodb://localhost/bookworm");
+app.use("/api/auth", auth);
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
