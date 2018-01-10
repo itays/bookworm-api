5357681d0d27ec939b5d66f28a1ed7eaba900f13
Itay Sidis <itaysidis@gmail.com>
2018-01-10 22:01:10 +0200

    ✨ User authentication

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import express from "express";
-import path from "path";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import auth from "./routes/auth";
+import express from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import auth from './routes/auth';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 app.use(bodyParser.json());
-mongoose.connect("mongodb://localhost/bookworm");
-app.use("/api/auth", auth);
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URL);
+app.use('/api/auth', auth);
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(8080, () => console.log("running on localhost:8080"));
+app.listen(8080, () => console.log('running on localhost:8080'));
