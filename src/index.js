f318bac6326402a15b330289b94cb18c3f931b6a
Itay Sidis <is874k@intl.att.com>
2018-01-23 18:29:20 +0200

    mongoose logging

--- src/index.js
+++ src/index.js
@@ -5,10 +5,22 @@ import mongoose from 'mongoose';
 import auth from './routes/auth';
 import dotenv from 'dotenv';
 dotenv.config();
-const app = express();
-app.use(bodyParser.json());
+
+/* Database */
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URL);
+const monDb = mongoose.connection;
+monDb.on('error', () => {
+  console.error('MongoDB Connection Error. Please make sure that is running');
+});
+
+monDb.on('open', () => {
+  console.info('Connected to mongodb:');
+});
+
+const app = express();
+
+app.use(bodyParser.json());
 app.use('/api/auth', auth);
 
 app.get('/*', (req, res) => {
